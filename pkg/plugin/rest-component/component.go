734360ab45c9ca7548be1873ae10819ee4c43e87
Chris Laprun <metacosm@gmail.com>
2020-02-29 18:50:32 +0100

    feat: record parameters from capability when created, use constants

--- pkg/plugin/rest-component/component.go
+++ pkg/plugin/rest-component/component.go
@@ -13,10 +13,17 @@ import (
 var _ framework.DependentResource = &component{}
 var gvk = v1beta12.SchemeGroupVersion.WithKind(v1beta12.Kind)
 
-const EndpointUrlKey = "ENDPOINT_URL"
+const (
+	EndpointUrlKey        = "ENDPOINT_URL"
+	EndpointOverrideParam = "halkyon.endpointKey"
+	ComponentParam        = "component"
+	PortParam             = "port"
+	ContextParam          = "context"
+)
 
 type component struct {
 	*framework.BaseDependentResource
+	params map[string]string
 }
 
 func NewComponent(owner framework.SerializableResource) *component {
@@ -24,7 +31,9 @@ func NewComponent(owner framework.SerializableResource) *component {
 	config.CheckedForReadiness = true
 	config.Created = false
 	config.Updated = true
-	p := &component{framework.NewConfiguredBaseDependentResource(owner, config)}
+	p := &component{BaseDependentResource: framework.NewConfiguredBaseDependentResource(owner, config)}
+	c := ownerAsCapability(p)
+	p.params = util.ParametersAsMap(c.Spec.Parameters)
 	return p
 }
 
@@ -33,9 +42,7 @@ func ownerAsCapability(res framework.DependentResource) *capability.Capability {
 }
 
 func (m *component) Name() string {
-	c := ownerAsCapability(m)
-	paramsMap := util.ParametersAsMap(c.Spec.Parameters)
-	return paramsMap["component"]
+	return m.params["component"]
 }
 
 func (m *component) NameFrom(underlying runtime.Object) string {
@@ -63,14 +70,12 @@ func (m *component) GetCondition(underlying runtime.Object, err error) *v1beta1.
 }
 
 func (m *component) GetDataMap() map[string][]byte {
-	c := ownerAsCapability(m)
-	paramsMap := util.ParametersAsMap(c.Spec.Parameters)
 	key := EndpointUrlKey
-	if override, ok := paramsMap["halkyon.endpointKey"]; ok {
+	if override, ok := m.params[EndpointOverrideParam]; ok {
 		key = override
 	}
 	result := map[string][]byte{
-		key: []byte(fmt.Sprintf("http://%s:%s%s", paramsMap["component"], paramsMap["port"], paramsMap["context"])),
+		key: []byte(fmt.Sprintf("http://%s:%s%s", m.params[ComponentParam], m.params[PortParam], m.params[ContextParam])),
 	}
 	return result
 }
